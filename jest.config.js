1f018bf3c835235bca350ad104f308140e505f46
Bruce Becker <brucellino@protonmail.ch>
2024-12-29 06:55:41 +0100

    chore: add jest for testing
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  preset: "ts-jest",
+  verbose: true,
   testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
