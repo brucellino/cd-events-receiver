f8b966063c88653a178bfee86a50237bbd0d2740
Bruce Becker <brucellino@protonmail.ch>
2024-12-29 06:10:52 +0100

    chore: update subject and context schemas
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- src/dev/cdevents/schemas.ts
+++ src/dev/cdevents/schemas.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
+/* 
+    Some types need to be created from CloudEvents: https://github.com/cloudevents/spec/blob/v1.0.2/cloudevents/spec.md#type-system
+    Also cdevent types: 
+    https://github.com/cdevents/spec/blob/v0.4.1/spec.md#types
 
-export const cdEventSubjectSchema = z.object({
-    id: z.string(),
-    source: z.string(),
-    type: z.string(),
-    content: z.unknown().optional()
-})
+    Object key names are by conventioned defined in CamelCase
+*/
+
 export const cdEventLinkSchema = z.object({
     linkType: z.string().optional(),
     linkKind: z.string().optional(),
@@ -13,14 +14,22 @@ export const cdEventLinkSchema = z.object({
         contextId: z.string().uuid().optional()
     }),
     tags: z.unknown().optional()
-})
+});
+
+export const cdEventSubjectSchema = z.object({
+    id: z.string(),
+    content: z.unknown(),
+    source: z.string().optional(),
+    type: z.string().optional(),
+});
 
 export const cdEventContextSchema = z.object({
-    version: z.string().regex(/^\d+\.\d+\.\d+$/), // semver format
     id: z.string().uuid(),
-    chainId: z.string().uuid(),
     type: z.string(),
+    source: z.string().url(),
+    version: z.string().regex(/^\d+\.\d+\.\d+$/), // semver format
     timestamp: z.string().datetime({ offset: true }), // ISO 8601 with timezone
+    chainId: z.string().uuid(),
     schemaUri: z.string().url(),
     links: z.array(cdEventLinkSchema).optional()
-})
+});
